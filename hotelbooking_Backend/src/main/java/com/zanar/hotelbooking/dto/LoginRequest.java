4e211a491b55b1c87cc03e28a94e942c32d2f583
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-09-18 14:16:22 +0530

    repo added

--- hotelbooking_Backend/src/main/java/com/zanar/hotelbooking/dto/LoginRequest.java
+++ hotelbooking_Backend/src/main/java/com/zanar/hotelbooking/dto/LoginRequest.java
@@ -1,2 +1,13 @@
-package com.zanar.hotelbooking.dto;public class LoginRequest {
+package com.zanar.hotelbooking.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
 }
