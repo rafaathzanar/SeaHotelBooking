50f7b7521f240ccfa8d69415e33c3bf2001c2983
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-11-06 12:40:25 +0530

    CorsConfig / JWTUtils

---
+++ hotelbooking_Backend/src/main/java/com/zanar/hotelbooking/exception/OurException.java
@@ -0,0 +1,9 @@
+package com.zanar.hotelbooking.exception;
+
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
---
+++ hotelbooking_Backend/src/main/java/com/zanar/hotelbooking/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.zanar.hotelbooking.service;
+
+
+
+import com.zanar.hotelbooking.exception.OurException;
+import com.zanar.hotelbooking.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
+}
